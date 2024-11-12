cdfa2defb2fcca9a632e161909a6752cdaa8d2f0
zhangjian2661 <14090993+zhangjian2661@user.noreply.gitee.com>
2024-11-11 21:48:42 -0500

    优化中文伪数据

--- chinese_pseudo/pesudo_single_font.py
+++ chinese_pseudo/pesudo_single_font.py
@@ -56,7 +56,7 @@ import matplotlib.font_manager
 import pickle
 
 # 定义用于保存生成图片的输出目录
-output_dir = "../../pseudo_chinese_images_1110"
+output_dir = "../../pseudo_chinese_images_1111_闫"
 os.makedirs(output_dir, exist_ok=True)
 
 # 读取字符字典
@@ -87,60 +87,28 @@ def is_blank_image(image, threshold=5):
     if diff_sum < threshold:
         return True
     return False
-# 定义要使用的字体列表
-# select_list = ["Wawati SC","Baoli SC"]
-# select_list = [
-#     "田英章硬笔楷书简体",
-#     "锐字云字库行草体",
-#     "于洪亮钢笔楷书简体",
-#     "汉仪平安行简",
-#     "钟齐吴嘉睿手写字",
-#     "庞中华硬笔行书",
-#     "汉仪伊宁隶简",
-#     "钟齐余好建行艺体",
-#     "方正字迹-子实行楷简体",
-#     "方正字迹-刘毅硬笔行书简体",
-#     "游狼美钢行书简",
-#     "方正字迹-刘郢硬笔简体",
-#     "陈旭东字体",
-#     "逐浪大雪钢笔体",
-#     "国祥手写体",
-#     "全新硬笔楷书简",
-#     "邢世新硬笔行书简体",
-#     "钟齐立强行书简",
-#     "汉仪晨妹子",
-#     "孙运和酷楷",
-#     "建刚草稿体",
-#     "钟齐陈伟勋硬笔行书字库",
-#     "方正字迹-刘毅硬笔楷书简体",
-#     "平方雨桐体",
-#     "陈继世硬笔行书",
-#     "禹卫硬笔字体",
-#     "字体管家印象体",
-#     "默陌信笺手写体",
-#     "蔡云汉硬笔行书简书法字体",
-#     "林志秀硬笔楷书",
-#     "于洪亮硬笔行楷手写字体",
-#     "钟齐山文丰手写体",
-#     "方正字迹-张亮硬笔行书简体",
-#     "方正瘦金书简体",
-#     "游狼软笔楷书",
-#     "方正硬笔楷书简体",
-#     "逐浪时尚钢笔体",
-#     "庞中华简体",
-#     "方正硬笔行书简体",
-#     "逐浪小雪钢笔体",
-#     "钟齐孟宪敏钢笔简体",
-#     "方正字迹-杜慧田硬笔楷书简体",
-#     "方正字迹-朱涛钢笔行书简体",
-#     "汉仪瘦金书简",
-#     "字体管家简行",
-#     "方正字迹-王伟钢笔行书简体",
-#     "字体管家青葱体",
-#     "新蒂绿豆体",
-#     "北风钢笔楷书"
-# ]
 
+def crop_off_whitespace(image):
+    # 转换为NumPy数组
+    # 将图像转换为灰度
+    gray_image = image.convert('L')
+    image_array = np.array(gray_image)
+    threshold = 220
+    # 计算每一行和每一列的灰度值之和
+    horizontal_sum = np.sum(image_array < threshold, axis=1)
+    vertical_sum = np.sum(image_array < threshold, axis=0)
+
+    # 找到上边界和下边界
+    top = np.argmax(horizontal_sum > 0)
+    bottom = len(horizontal_sum) - np.argmax(horizontal_sum[::-1] > 0)
+
+    # 找到左边界和右边界
+    left = np.argmax(vertical_sum > 0)
+    right = len(vertical_sum) - np.argmax(vertical_sum[::-1] > 0)
+
+    # 裁剪图像
+    cropped_image = image.crop((left, top, right, bottom))
+    return cropped_image
 
 # 设置图片尺寸和字体大小
 image_width = 300
@@ -150,7 +118,7 @@ font_size = 70
 # # 用户字体目录（请根据实际路径进行修改）
 #user_font_dir = os.path.expanduser("/Users/zhangjian/Library/Fonts")
 # user_font_dir = os.path.expanduser("/System/Library/Fonts")
-user_font_dir = os.path.expanduser("/Users/zhangjian/Downloads/free-font-master/assets/font/中文/selected_test/")
+user_font_dir = os.path.expanduser("/Users/zhangjian/Downloads/free-font-master/assets/font/中文/selected_hw/")
 #user_font_dir = os.path.expanduser("/Users/zhangjian/Downloads/free-font-master/assets/font/中文/selected/")
 
 # 获取系统中已安装的字体列表
@@ -208,49 +176,44 @@ print(f"有效字体数量: {len(valid_fonts)}")
 # 遍历文本列表，为每个文本使用不同的字体生成图片并保存
 
 for font in tqdm(valid_fonts):
-        for text_group in texts:
-            # image = Image.new("RGB", (image_width, image_height), color="white")
-            # draw = ImageDraw.Draw(image)
+    for text_group in texts:
+        if text_group != "闫":
+            continue
+        # image = Image.new("RGB", (image_width, image_height), color="white")
+        # draw = ImageDraw.Draw(image)
 
-            # 计算文本的大小和位置
-            #text_width, text_height = draw.textsize(text_group, font=font[0])
-            #text_width0, text_height_0 = draw.textsize(" ", font=font[0])
-            text_width, text_height = font[0].getsize(text_group)
-            # 动态计算图片尺寸
-            if text_width == 0 or text_height == 0:
-                continue
+        # 计算文本的大小和位置
+        #text_width, text_height = draw.textsize(text_group, font=font[0])
+        #text_width0, text_height_0 = draw.textsize(" ", font=font[0])
+        text_width, text_height = font[0].getsize(text_group)
+        # 动态计算图片尺寸
+        if text_width == 0 or text_height == 0:
+            continue
 
-            # 调整图片尺寸以适应文本
-            image_width = text_width + 5
-            image_height = text_height + 5
-            image = Image.new("RGB", (image_width, image_height), color="white")
-            draw = ImageDraw.Draw(image)
+        # 调整图片尺寸以适应文本
+        image_width = text_width + 10
+        image_height = text_height + 10
+        image = Image.new("RGB", (image_width, image_height), color="white")
+        draw = ImageDraw.Draw(image)
 
-            # 计算文本位置
-            x = (image_width - text_width) / 2
-            y = (image_height - text_height) / 2
+        # 计算文本位置
+        x = (image_width - text_width) / 2
+        y = (image_height - text_height) / 2
 
-            # 绘制文本
-            # if 语句确保字体没问题，否则可能出现空白图片
-            if font[0].getsize(text_group)[0] > 0:
-                draw.text((x, y), text_group, fill="black", font=font[0])
-            else:
-                continue
+        # 绘制文本
+        # if 语句确保字体没问题，否则可能出现空白图片
+        if font[0].getsize(text_group)[0] > 0:
+            draw.text((x, y), text_group, fill="black", font=font[0])
+        else:
+            continue
 
 
-            # Add underline
-
-            #under_line_hight = max(2, int(image_height / 32))
-            #underline_y = random.randint(int(y + 0.9 * text_height), int(y + 1.1 * text_height)) # Adjust 2 according
-            # to your
-            # preference
-            #underline_y = min(underline_y, image_height - 10)
-            #draw.line([(x, underline_y), (x + text_width, underline_y)], fill="black",
-            #          width=under_line_hight)  # Adjust width as needed
-            if not is_blank_image(image):
-            # 保存图像
-                image_filename = os.path.join(output_dir, f"{font[1]}_{text_group}.jpg")
-                image.save(image_filename)
+        if not is_blank_image(image):
+        # 保存图像
+            # 剪裁四周多余空白
+            #image = crop_off_whitespace(image)
+            image_filename = os.path.join(output_dir, f"{font[1]}_{text_group}.jpg")
+            image.save(image_filename)
 
             # 打印生成的信息
             #print(f"生成图像 {image_filename}，文本: {text_group}，字体: {font[1]}")
--- chinese_pseudo/pseudo_line_with_delete.py
+++ chinese_pseudo/pseudo_line_with_delete.py
@@ -186,6 +186,27 @@ def generate_random_line(length,off_set,random_seq=False):
         random.shuffle(temp_list)
         return ''.join(temp_list)
 
+def crop_off_whitespace(image):
+    # 转换为NumPy数组
+    # 将图像转换为灰度
+    gray_image = image.convert('L')
+    image_array = np.array(gray_image)
+    threshold = 230
+    # 计算每一行和每一列的灰度值之和
+    horizontal_sum = np.sum(image_array < threshold, axis=1)
+    vertical_sum = np.sum(image_array < threshold, axis=0)
+
+    # 找到上边界和下边界
+    top = np.argmax(horizontal_sum > 0)
+    bottom = len(horizontal_sum) - np.argmax(horizontal_sum[::-1] > 0)
+
+    # 找到左边界和右边界
+    left = np.argmax(vertical_sum > 0)
+    right = len(vertical_sum) - np.argmax(vertical_sum[::-1] > 0)
+
+    # 裁剪图像
+    cropped_image = image.crop((left, top, right, bottom))
+    return cropped_image
 
 def load_local_images(image_directory):
     """这个函数根据单子数据添加到一个字典结构中"""
@@ -269,11 +290,19 @@ def create_handwritten_number_image(line_chars, output_path, mnist_data, font_st
             single_image = single_image.resize((50, 70), Image.ANTIALIAS)
             #single_image = single_image.resize((50, 70), Image.Resampling.LANCZOS)
 
-        single_width, single_height = single_image.size
+        # single_width, single_height = single_image.size
+        # # 加入划痕
+        # if random.choice(range(20)) == 0:
+        #     single_image = apply_scratches(single_image)
+        #     list_of_text[i] = 'x'
+
         # 加入划痕
         if random.choice(range(20)) == 0:
+            single_image = crop_off_whitespace(single_image)
             single_image = apply_scratches(single_image)
             list_of_text[i] = 'x'
+        single_width, single_height = single_image.size
+
         #if cell_width - single_width >= 0:
         offset_x = random.randint(0, cell_width - single_width)
         ##else:
@@ -285,17 +314,398 @@ def create_handwritten_number_image(line_chars, output_path, mnist_data, font_st
         paste_position = (off_set_position + offset_x, offset_y)
         off_set_position += offset_x + single_width
         image.paste(single_image, paste_position)
+    # 切边
+    image = crop_off_whitespace(image)
+    width, height = image.size
 
     draw = ImageDraw.Draw(image)
     if random.choice(range(2)) == 0:
         underline_y = height - random.randint(3,7)  # 下划线的位置
         draw.line([(0, underline_y), (width, underline_y)], fill=0, width=2)
 
+
     # 添加边距
-    left_margin = random.randint(2, 10)
-    right_margin = random.randint(2, 10)
-    top_margin = random.randint(2, 10)
-    bottom_margin = random.randint(2, 10)
+    # """
+    # # 作者：张健
+    # # 时间：2024.10.20
+    # # 这个模块制造伪的行数据，基于单个中文字，随机添加删除符号
+    # # """
+    # #
+    # # from PIL import Image, ImageDraw
+    # # import numpy as np
+    # # import random
+    # # # import string
+    # # # import tensorflow as tf
+    # # import time
+    # # import multiprocessing
+    # # import os
+    # # from tqdm import tqdm
+    # # import cv2  # OpenCV库，用于更快的图像处理
+    # # from gen_scratch import apply_scratches
+    # # import pickle
+    # #
+    # # # 加载需要制作哪些汉字的字典
+    # # with open('chinese_data1018/char_dict', 'rb') as f:
+    # #     char_dict = pickle.load(f)
+    # # # 从字符字典中提取字符
+    # # dict_list = list(char_dict.keys())
+    # #
+    # # def generate_random_line(length):
+    # #     return ''.join(random.choices(dict_list, k=length))
+    # #
+    # #
+    # # def load_local_images(image_directory):
+    # #     """这个函数根据单子数据添加到一个字典结构中"""
+    # #     mnist_data = {}
+    # #     for filename in os.listdir(image_directory):
+    # #         if filename.endswith('.jpg'):
+    # #             # 根据文件名称提取标注，中文标注第一部分是字体名称，第二部分是标注内容
+    # #             font_name, label = filename.split('_', 1)
+    # #             label = label.split('.')[0]
+    # #             filepath = os.path.join(image_directory, filename)
+    # #             if label not in mnist_data:
+    # #                 mnist_data[label] = []
+    # #             image = Image.open(filepath).convert('L')  # 转为灰度图
+    # #             mnist_data[label].append(np.array(image))
+    # #     return mnist_data
+    # #
+    # #
+    # # def create_handwritten_number_image(line_chars, output_path, mnist_data):
+    # #     list_of_text = list(line_chars)
+    # #     width = 70 * len(line_chars)
+    # #     height = 70
+    # #     cell_width = width // len(line_chars)
+    # #
+    # #     # 创建白色背景的新图像
+    # #     image = Image.new('L', (width, height), 255)
+    # #     draw = ImageDraw.Draw(image)
+    # #
+    # #     # rand_dash_all = random.randint(6, 9)
+    # #     # rand_dash_inter = random.randint(1, 3)
+    # #     # rand_dash_select = 1
+    # #
+    # #     for i, text_char in enumerate(line_chars):
+    # #
+    # #         if text_char not in mnist_data:
+    # #             print(f"未找到字符的图像：{text_char}")
+    # #             continue
+    # #
+    # #         char_images = mnist_data[text_char]
+    # #         single_image = char_images[np.random.choice(len(char_images))]
+    # #
+    # #         # 调整颜色和大小
+    # #         scaled_w = int(width / len(line_chars) * random.uniform(0.85, 1.0))
+    # #         scaled_h = int(height * random.uniform(0.85, 1.0))
+    # #         single_image = cv2.resize(single_image, (scaled_w, scaled_h), interpolation=cv2.INTER_LINEAR)
+    # #         single_image = Image.fromarray(single_image)
+    # #
+    # #         # 加入划痕
+    # #         if random.choice(range(11)) == 0:
+    # #             single_image = apply_scratches(single_image)
+    # #             list_of_text[i] = 'x'
+    # #
+    # #         offset_x = random.randint(0, width // len(line_chars) - scaled_w)
+    # #         offset_y = random.randint(0, height - scaled_h)
+    # #         paste_position = (i * cell_width + offset_x, offset_y)
+    # #         image.paste(single_image, paste_position)
+    # #
+    # #         # # 绘制竖线
+    # #         # if rand_dash_select == 1 and i > 0:
+    # #         #     for y_dash in range(0, height, rand_dash_all):
+    # #         #         draw.line([(i * cell_width, y_dash), (i * cell_width, y_dash + rand_dash_inter)], fill=0)
+    # #         # elif rand_dash_select == 2 and i > 0:
+    # #         #     draw.line([(i * cell_width, 0), (i * cell_width, height)], fill=0)
+    # #
+    # #     #draw.rectangle([0, 0, width - 1, height - 1], outline=0, width=3)
+    # #
+    # #     # 添加边距
+    # #     left_margin = 15
+    # #     right_margin = 15
+    # #     top_margin = 15
+    # #     bottom_margin = 15
+    # #     larger_width = width + left_margin + right_margin
+    # #     larger_height = height + top_margin + bottom_margin
+    # #     larger_image = Image.new('L', (larger_width, larger_height), 255)
+    # #     larger_image.paste(image, (left_margin, top_margin))
+    # #
+    # #     # random_angle = np.clip(np.random.normal(0, 5), -3, 3)
+    # #     # rotated_img = larger_image.rotate(random_angle, fillcolor=(255))
+    # #
+    # #     # 保存图像
+    # #     timestamp = int(time.time())
+    # #     text_new = "".join(list_of_text)
+    # #     output_file = f'{output_path}{timestamp}_{text_new}.jpg'
+    # #     #rotated_img.save(output_file)
+    # #
+    # #     larger_image.save(output_file)
+    # #
+    # # def process_image_wrapper(args):
+    # #     output_path, text, mnist_data = args
+    # #     create_handwritten_number_image(text, output_path, mnist_data)
+    # #     return output_path
+    # #
+    # #
+    # # if __name__ == '__main__':
+    # #     random.seed(42)
+    # #     image_directory = '/Users/zhangjian/PycharmProjects/pseudo_chinese_print_images'
+    # #
+    # #     # 加载单个汉字图片
+    # #     mnist_data = load_local_images(image_directory)
+    # #     output_paths_and_texts = []
+    # #     for i in range(200000):
+    # #         length = random.randint(15, 20)
+    # #         # 生成一串连续的文本
+    # #         text = generate_random_line(length)
+    # #         timestamp = int(time.time()) + i
+    # #         output_path = f'../../psudo_chinese_data/gen_line_print_data/'
+    # #         output_paths_and_texts.append((output_path, text))
+    # #
+    # #     num_processes = multiprocessing.cpu_count()
+    # #
+    # #     with multiprocessing.Pool(processes=num_processes) as pool:
+    # #         results = list(tqdm(pool.imap_unordered(process_image_wrapper,
+    # #                                                 [(path, text, mnist_data) for path,
+    # text in output_paths_and_texts]),
+    # #                             total=len(output_paths_and_texts)))
+    # #
+    # #     # # 单线程处理
+    # #     # for output_path, text in tqdm(output_paths_and_texts):
+    # #     #     process_image_wrapper((output_path, text, mnist_data))
+    #
+    #
+    # """
+    # 作者：张健
+    # 时间：2024.10.20
+    # 这个模块制造伪的行数据，基于单个中文字，随机添加删除符号
+    # """
+    #
+    # from PIL import Image, ImageDraw
+    # import numpy as np
+    # import random
+    # import time
+    # import multiprocessing
+    # import os
+    # from tqdm import tqdm
+    # import cv2  # OpenCV库，用于更快的图像处理
+    # from gen_scratch import apply_scratches
+    # from image_operation import *
+    # import pickle
+    #
+    # # 加载需要制作哪些汉字的字典
+    # # with open('chinese_data1018/char_dict', 'rb') as f:
+    # #     char_dict = pickle.load(f)
+    # # # 从字符字典中提取字符
+    # # dict_list = list(char_dict.keys())
+    # char_dict = {}
+    # with open('char_dict.txt', 'r', encoding='utf-8') as f:
+    #     for line in f:
+    #         char, code = line.strip().split('\t')  # 按制表符分割
+    #         char_dict[char] = int(code)  # 将编码转换为整数
+    #
+    # dict_list = list(char_dict.keys())
+    #
+    # def generate_random_line(length,off_set,random_seq=False):
+    #     if random_seq:
+    #         return ''.join(random.choices(dict_list, k=length))
+    #
+    #     else:
+    #         temp_list = dict_list[off_set : min(off_set + length,len(dict_list))]
+    #         # 打乱字符顺序
+    #         random.shuffle(temp_list)
+    #         return ''.join(temp_list)
+    #
+    # def crop_off_whitespace(image):
+    #     # 转换为NumPy数组
+    #     # 将图像转换为灰度
+    #     gray_image = image.convert('L')
+    #     image_array = np.array(gray_image)
+    #     threshold = 230
+    #     # 计算每一行和每一列的灰度值之和
+    #     horizontal_sum = np.sum(image_array < threshold, axis=1)
+    #     vertical_sum = np.sum(image_array < threshold, axis=0)
+    #
+    #     # 找到上边界和下边界
+    #     top = np.argmax(horizontal_sum > 0)
+    #     bottom = len(horizontal_sum) - np.argmax(horizontal_sum[::-1] > 0)
+    #
+    #     # 找到左边界和右边界
+    #     left = np.argmax(vertical_sum > 0)
+    #     right = len(vertical_sum) - np.argmax(vertical_sum[::-1] > 0)
+    #
+    #     # 裁剪图像
+    #     cropped_image = image.crop((left, top, right, bottom))
+    #     return cropped_image
+    #
+    # def load_local_images(image_directory):
+    #     """这个函数根据单子数据添加到一个字典结构中"""
+    #     mnist_data = {}
+    #     font_style = []
+    #     #files = sorted(os.listdir(image_directory))
+    #     files = os.listdir(image_directory)
+    #     filenames = [f for f in files if f.endswith('.jpg')]
+    #     for filename in tqdm(filenames[:1000], desc="加载图像"):
+    #         font_name, label = filename.split('_', 1)
+    #         label = label.split('.')[0]
+    #         if font_name not in font_style:
+    #             font_style.append(font_name)
+    #         filepath = os.path.join(image_directory, filename)
+    #         image = Image.open(filepath).convert('L')  # 转为灰度图
+    #         # if label not in mnist_data:
+    #         #     mnist_data[label] = []
+    #         # mnist_data[label].append(np.array(image))
+    #         # 初始化字典结构
+    #         if label not in mnist_data:
+    #             mnist_data[label] = {}
+    #
+    #         # 将图像数据存入相应的标签和字体名下
+    #         mnist_data[label][font_name] = np.array(image)
+    #         #font_style = list(set(font_style))
+    #     return font_style, mnist_data
+    #
+    # def create_handwritten_number_image(line_chars, output_path, mnist_data, font_style, random_font=False):
+    #     list_of_text = list(line_chars)
+    #     width = 50 * len(line_chars)
+    #     height = 70
+    #     image = Image.new('L', (width, height), 255)
+    #
+    #     # 随机选择一次所有字符的图像
+    #     selected_images = []
+    #     style = random.choice(font_style)
+    #     for i_c, char in enumerate(line_chars):
+    #         if char in mnist_data:
+    #             char_images = mnist_data[char]
+    #             # if random_font:
+    #             #     selected_image = char_images[random.choice(font_style)]
+    #             # else:
+    #             # #selected_image = char_images[np.random.choice(len(char_images))]
+    #             #     selected_image = char_images[style]
+    #             if random_font:
+    #                 selected_image = char_images.get(random.choice(font_style), char_images.get(random.choice(list(
+    #                 char_images.keys()))))
+    #             else:
+    #                 selected_image = char_images.get(style, char_images.get(random.choice(list(char_images.keys()))))
+    #             selected_images.append(selected_image)
+    #         else:
+    #             print(f"未找到字符的图像：{char}")
+    #             #raise
+    #             #selected_images.append(np.zeros((height, width)))  # 如果找不到，填充空白图像
+    #             selected_images.append(np.ones((height, width)) * 255)  # 如果找不到，填充白色图
+    #             list_of_text[i_c] = " "
+    #     # 粘贴图像
+    #     cell_width = width // len(line_chars)
+    #     off_set_position = 0
+    #     # 加入多样性？
+    #     random_flag = False
+    #     if random.choice(range(2)) == 0:
+    #         random_flag = True
+    #     for i, single_image in enumerate(selected_images):
+    #         # 调整颜色和大小
+    #         scale_ratio = random.uniform(0.8, 1.0)
+    #         scaled_w = int(cell_width * scale_ratio)
+    #         scaled_h = int(height * scale_ratio)
+    #         single_image = cv2.resize(single_image, (scaled_w, scaled_h), interpolation=cv2.INTER_LINEAR)
+    #         single_image = Image.fromarray(single_image)
+    #         # 透视变换
+    #         if random_flag and random.choice(range(2)) == 0:
+    #             single_image = apply_perspective_transform(single_image)
+    #         # 应用旋转变换
+    #         #  # 旋转角度，可以调整
+    #         if random_flag and random.choice(range(2)) == 0:
+    #             angle_ratio = random.uniform(-1.0, 1.0)
+    #             angle = 10 * angle_ratio
+    #             single_image = rotate_text_image(single_image, angle)
+    #         single_width, single_height = single_image.size
+    #         if single_height > 70 or single_width > 50:
+    #             single_image = single_image.resize((50, 70), Image.ANTIALIAS)
+    #             #single_image = single_image.resize((50, 70), Image.Resampling.LANCZOS)
+    #
+    #         single_width, single_height = single_image.size
+    #         # 加入划痕
+    #         if random.choice(range(20)) == 0:
+    #             single_image = apply_scratches(single_image)
+    #             list_of_text[i] = 'x'
+    #         #if cell_width - single_width >= 0:
+    #         offset_x = random.randint(0, cell_width - single_width)
+    #         ##else:
+    #         #    offset_x = single_width - cell_width
+    #
+    #         offset_y = random.randint(int(0.5*(height - single_height)), height - single_height)
+    #
+    #         #paste_position = (i * cell_width + offset_x, offset_y)
+    #         paste_position = (off_set_position + offset_x, offset_y)
+    #         off_set_position += offset_x + single_width
+    #         image.paste(single_image, paste_position)
+    #     # 切边
+    #     image = crop_off_whitespace(image)
+    #     width, height = image.size
+    #
+    #     draw = ImageDraw.Draw(image)
+    #     if random.choice(range(2)) == 0:
+    #         underline_y = height - random.randint(3,7)  # 下划线的位置
+    #         draw.line([(0, underline_y), (width, underline_y)], fill=0, width=2)
+    #
+    #
+    #     # 添加边距
+    #     left_margin = 0#random.randint(2, 10)
+    #     right_margin = 0#random.randint(2, 10)
+    #     top_margin = 0#random.randint(2, 10)
+    #     bottom_margin = 0#random.randint(2, 10)
+    #     larger_width = width + left_margin + right_margin
+    #     larger_height = height + top_margin + bottom_margin
+    #     larger_image = Image.new('L', (larger_width, larger_height), 255)
+    #     larger_image.paste(image, (left_margin, top_margin))
+    #
+    #     # 保存图像
+    #     timestamp = int(time.time())
+    #     text_new = "".join(list_of_text)
+    #     output_file = f'{output_path}{timestamp}_{text_new}.jpg'
+    #     larger_image.save(output_file)
+    #
+    # def process_image_wrapper(args):
+    #     output_path, text, mnist_data,font_style = args
+    #     create_handwritten_number_image(text, output_path, mnist_data, font_style)
+    #     return output_path
+    #
+    # if __name__ == '__main__':
+    #     random.seed(42)
+    #     image_directory = '../../pseudo_chinese_images_1110'
+    #     output_path = f'../../psudo_chinese_data/gen_line_print_data_1111_test/'
+    #     random_font = False
+    #     random_seq = True
+    #     os.makedirs(output_path, exist_ok=True)
+    #
+    #     # 加载单个汉字图片
+    #     font_style, mnist_data = load_local_images(image_directory)
+    #     output_paths_and_texts = []
+    #     off_set = 0
+    #     for i in tqdm(range(1000)):
+    #         length = random.randint(15, 20)
+    #         # 生成一串连续的文本
+    #         text = generate_random_line(length, off_set, random_seq)
+    #         off_set += length
+    #         if off_set > len(dict_list):
+    #             off_set = 0
+    #         if len(text) == 0:
+    #             continue
+    #         timestamp = int(time.time()) + i
+    #         #output_paths_and_texts.append((output_path, text))
+    #         create_handwritten_number_image(text, output_path, mnist_data, font_style, random_font)
+    #
+    #     # num_processes = multiprocessing.cpu_count()
+    #     #
+    #     # with multiprocessing.Pool(processes=num_processes) as pool:
+    #     #     results = list(tqdm(pool.imap_unordered(process_image_wrapper,
+    #     #                                             [(path, text, mnist_data) for path,
+    #     text in output_paths_and_texts]),
+    #     #                         total=len(output_paths_and_texts)))
+    #     # 单线程处理
+    #
+    #     # for output_path, text in tqdm(output_paths_and_texts):
+    #     #     process_image_wrapper((output_path, text, mnist_data, font_style))
+    left_margin = 0#random.randint(2, 10)
+    right_margin = 0#random.randint(2, 10)
+    top_margin = 0#random.randint(2, 10)
+    bottom_margin = 0#random.randint(2, 10)
     larger_width = width + left_margin + right_margin
     larger_height = height + top_margin + bottom_margin
     larger_image = Image.new('L', (larger_width, larger_height), 255)
@@ -315,7 +725,7 @@ def process_image_wrapper(args):
 if __name__ == '__main__':
     random.seed(42)
     image_directory = '../../pseudo_chinese_images_1110'
-    output_path = f'../../psudo_chinese_data/gen_line_print_data_1110/'
+    output_path = f'../../psudo_chinese_data/gen_line_print_data_1111_test/'
     random_font = False
     random_seq = True
     os.makedirs(output_path, exist_ok=True)
